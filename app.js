a1ecfd9235f33382d0c45176e1aad78ac5500844
Csaba Balint <csababalint@csabas-mbp-2.home>
2021-12-12 19:09:02 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+var cors = require('cors')
+const api_helper = require('./API_helper')
+const app = express()
+const port = 3000
+const key = "41b766d0cd0542d1b34a4ab70409abdf";
+
+app.use(cors())
+
+app.get('/', (req, res) => res.send('Welcome to Make REST API Calls In Express!'))
+
+app.get('/getAPIResponse/topHeadlines', cors(), (req, res) => {
+	api_helper.make_API_call(`https://newsapi.org/v2/top-headlines?country=us&page=pageSize=40&category=&apiKey=${key}`)
+		.then(response => {
+			res.json(response)
+		})
+		.catch(error => {
+			res.send(error)
+		})
+})
+
+app.get('/getAPIResponse/category', cors(), (req, res) => {
+	const { category } = req.query;
+	console.log({ category })
+
+	api_helper.make_API_call(`https://newsapi.org/v2/top-headlines?country=us&pageSize=30&category=${category}&apiKey=${key}`)
+		.then(response => {
+			res.json(response)
+		})
+		.catch(error => {
+			res.send(error)
+		})
+})
+
+app.get('/getAPIResponse/topic', cors(), (req, res) => {
+	const { topic } = req.query;
+	console.log({ topic })
+
+	api_helper.make_API_call(`https://newsapi.org/v2/everything?q=${topic}&pageSize=100&apiKey=${key}`)
+		.then(response => {
+			res.json(response)
+		})
+		.catch(error => {
+			res.send(error)
+		})
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
+
+module.exports = app
